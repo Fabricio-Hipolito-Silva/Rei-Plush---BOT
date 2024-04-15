8433ed4acd2168ef99bf7d266549e2820af6833d
Fafas <fabriciohipvpaul@gmail.com>
2024-04-15 19:04:33 -0300

    Types
    Fiz um array dos tipos do pokemon, e tá funcionando.

--- commands/utility/pokedex.js
+++ commands/utility/pokedex.js
@@ -1,5 +1,7 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fetch = require('node-fetch'); 
+const { EmbedBuilder } = require('discord.js');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -11,12 +13,37 @@ module.exports = {
                 .setRequired(true)),
 
 	async execute(interaction) {
+
 	const pokemon = interaction.options.getString("nome_pokemon")
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     const poke_data = await response.json()
-    const poke_name = poke_data.id
-    await interaction.reply(`O nome do pokemon é ${poke_name}`)
-    console.log (poke_data.name)
+    let poke_name = poke_data.name; // Atribuição correta
+    poke_name = poke_name.charAt(0).toUpperCase() + poke_name.slice(1);
+    const poke_image = poke_data.sprites.other["official-artwork"].front_default;
 
+	const descUrl = poke_data.species.url;
+	const descResponse = await fetch(descUrl);
+	const descData = await descResponse.json();
+	const poke_desc = descData.flavor_text_entries["0"].flavor_text;
+	
+	let poke_types = poke_data.types.map(type => type.type.name);
+	console.log(poke_types);
+	poke_types = poke_types.map(type => type.charAt(0).toUpperCase() + type.slice(1));
+
+
+
+const pokedex = new EmbedBuilder()
+	.setColor('Red')
+	.setTitle(`${poke_name}`)
+	.setDescription(`${poke_desc}`)
+	.setThumbnail('https://64.media.tumblr.com/9b521d7ea0df3175f4821022c504e231/716cd31c60495dbd-05/s250x400/8bcd348f1e026c7df95aee10f935f98249b0da16.pnj')
+	.setImage(`${poke_image}`)
+	.addFields(
+		{ name: 'Type', value: `${poke_types}`, inline: true },
+	)
+    await interaction.reply({embeds: [pokedex]})
 	},
 };
+
+
+
