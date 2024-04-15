db8a3f279ab8f1b155fe9ce9a804b8f189bdac88
Fafas <fabriciohipvpaul@gmail.com>
2024-04-15 19:57:12 -0300

    Weakness Funcionando
    Weakness funcionam agora, quer dizer, funcionam mais ou menos

--- commands/utility/pokedex.js
+++ commands/utility/pokedex.js
@@ -24,14 +24,20 @@ module.exports = {
 	const descUrl = poke_data.species.url;
 	const descResponse = await fetch(descUrl);
 	const descData = await descResponse.json();
-	const poke_desc = descData.flavor_text_entries["0"].flavor_text;
+	const poke_desc = descData.flavor_text_entries.find(entry => entry.language.name === 'en').flavor_text;
 	
 	let poke_types = poke_data.types.map(type => type.type.name);
 	console.log(poke_types);
 	poke_types = poke_types.map(type => type.charAt(0).toUpperCase() + type.slice(1));
 
+	const typeUrl = poke_data.types["0"].type.url
+	const typeResponse = await fetch(typeUrl)
+	const typeData = await typeResponse.json()
+	let double_dmg_names = typeData.damage_relations.double_damage_from.map(double_dmg_from=> double_dmg_from.name)
+	double_dmg_names = double_dmg_names.map(double_damage_from => double_damage_from.charAt(0).toUpperCase() + double_damage_from.slice(1))
 
 
+		
 const pokedex = new EmbedBuilder()
 	.setColor('Red')
 	.setTitle(`${poke_name}`)
@@ -39,7 +45,8 @@ const pokedex = new EmbedBuilder()
 	.setThumbnail('https://64.media.tumblr.com/9b521d7ea0df3175f4821022c504e231/716cd31c60495dbd-05/s250x400/8bcd348f1e026c7df95aee10f935f98249b0da16.pnj')
 	.setImage(`${poke_image}`)
 	.addFields(
-		{ name: 'Type', value: `${poke_types}`, inline: true },
+		{ name: 'Type', value: `${poke_types}`, inline: false },
+		{ name: "Weakness", value: `${double_dmg_names}`, inline: false},
 	)
     await interaction.reply({embeds: [pokedex]})
 	},
